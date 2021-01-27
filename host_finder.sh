77afd53235bbe14bd9c6182126c94817769d0b9e
Harishbabu Rengaraj <58879939+reharish@users.noreply.github.com>
2021-01-27 11:29:01 +0530

    Update host_finder.sh

--- host_finder.sh
+++ host_finder.sh
@@ -5,36 +5,29 @@
 # Description : This script will run through your network ip addresses,so that,
 #		it will collect IP address of hosts which are UP now.
 
-
-#echo $MyIpAdd
-
 is_host_up () {
-	ping -c 1 $1 > /dev/null
-	[ $? -eq 0 ] && echo $1 : Host is up 
+        ping -c 1 $1 > /dev/null
+        [ $? -eq 0 ] && echo $1 : Host is up 
 }
 
-if [ $# == 1 ]
+if [ $# -eq 1 ]
 then
-	MyIpAdd=$(ifconfig $1 | tail -n 8 | head -n 1 | cut -d " " -f 10)
-	#echo $MyIpAdd
-	MyNetwork=$(ifconfig $1 | tail -n 8 | head -n 1 | cut -d " " -f 10 | cut -d "." -f 1-3)
-	if [ "$MyIpAdd" != "" ]
-	then
-		for i in $MyNetwork.{1..254}
-		do
-		if [ $i != $MyIpAdd ]
-		then
-		is_host_up $i & disown
-		else
-		echo "$i : Your IP address"
-		fi
-		done
-	else
-		echo "unable to get IP Address"
-		echo "check Your Network Interface"
-		echo " "
-	fi
+MyIpAdd=$(ifconfig $1 | tail -n 8 | head -n 1 | cut -d " " -f 10)
+        #echo $MyIpAdd
+        MyNetwork=$(ifconfig $1 | tail -n 8 | head -n 1 | cut -d " " -f 10 | cut -d "." -f 1-3)
+for i in $MyNetwork.{1..254}
+do
+   if [ $i != $MyIpAdd ]
+   then
+   is_host_up $i & disown
+   else
+       echo "$i : Your IP address"
+   fi
+done
+	exit 0
 else
-	echo "Invalid Arguments"
-	echo "USAGE : $0 <NetworkInterface i,e wlan0,eth0,wlp2s0,enp1s0 >" 
+	echo "Invalid Argument"
+	echo "USAGE : $0 <NetworkInterface i,e wlan0,eth0,wlp2s0,enp1s0 >"
+	exit 111
+	echo Terminated With error code $?
 fi
